2a4c7f4628dde5d10f9f7b1b9f5bbb9c4251c7ca
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-11-06 17:55:17 +0300

    create Person, Student and Teacher classes

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'Person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(id, age, specialization, name = 'unknown', parent_permission: true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
