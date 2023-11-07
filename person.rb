fb3dc3e315b6767c40429c99f0fd720bab627755
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-11-07 17:50:31 +0300

    review changes

--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super(@nameable = name)
     @name = name
     @age = age
     @parent_permission = parent_permission
