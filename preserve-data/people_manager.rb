4f0ec4e6b9a635c5ba4b0af45bb6c2a5f5663ac3
newtonah0 <ezmh015@gmail.com>
2023-11-16 17:41:37 +0300

    fix linters errors

--- preserve-data/people_manager.rb
+++ preserve-data/people_manager.rb
@@ -1,18 +1,22 @@
 require 'json'
 class PeopleManager
   attr_accessor :people
+
   def initialize
     @people = []
   end
+
   def load_people_data
     load_people
   end
+
   def save_people_data
     save_people
     puts 'Person saved!'
   rescue StandardError => e
     puts "Error saving person data: #{e.message}"
   end
+
   def save_people
     File.open('people.json', 'w') do |file|
       file.puts @people.map { |person|
@@ -24,9 +28,12 @@ class PeopleManager
                 }.to_json
     end
   end
+
   private
+
   def load_people
     return unless File.exist?('people.json') && !File.empty?('people.json')
+
     json_str = File.read('people.json')
     @people = JSON.parse(json_str).map do |person_data|
       Person.new(person_data['id'], person_data['age'], person_data['name'])
