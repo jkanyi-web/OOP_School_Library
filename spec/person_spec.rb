149d4cf65a5be1fb46f9d757ddbfe968ffdd1f73
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-11-17 15:36:09 +0300

    review change to add more tests to person_spec.rb

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+require 'date'
 
 describe Person do
   describe '#initialize' do
@@ -16,6 +17,48 @@ describe Person do
       person = Person.new(2, 16)
 
       expect(person.instance_variable_get(:@name)).to eq('unknown')
+    end
+  end
+
+  describe '#can_use_services?' do
+    context 'when the person is of age' do
+      it 'returns true' do
+        person = Person.new(1, 20, 'Alice', parent_permission: false)
+        expect(person.can_use_services?).to eq(true)
+      end
+    end
+
+    context 'when the person is not of age but has parent permission' do
+      it 'returns true' do
+        person = Person.new(2, 16, 'Bob', parent_permission: true)
+        expect(person.can_use_services?).to eq(true)
+      end
+    end
+
+    context 'when the person is not of age and does not have parent permission' do
+      it 'returns false' do
+        person = Person.new(3, 15, 'Charlie', parent_permission: false)
+        expect(person.can_use_services?).to eq(false)
+      end
+    end
+  end
+
+  describe '#add_rental' do
+    it 'adds a rental to the person' do
+      person = Person.new(1, 18, 'David', parent_permission: true)
+      book = Book.new(1, 'The Great Gatsby', 'F. Scott Fitzgerald')
+      date = Date.new(2023, 11, 1)
+
+      expect { person.add_rental(date, book) }.to change { person.rentals.size }.by(2)
+    end
+  end
+
+  describe '#to_hash' do
+    it 'returns a hash with person attributes' do
+      person = Person.new(1, 25, 'Eve', parent_permission: true)
+      expected_hash = { 'id' => 1, 'age' => 25, 'name' => 'Eve' }
+
+      expect(person.to_hash).to eq(expected_hash)
     end
   end
 end
